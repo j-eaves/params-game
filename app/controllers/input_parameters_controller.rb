8230f9c2ddb482ac2f7409445f1d72e7e3554673
j-eaves <jdeaves@gmail.com>
2017-03-09 10:34:40 -0500

    Add some comments on functionality

--- app/controllers/input_parameters_controller.rb
+++ app/controllers/input_parameters_controller.rb
@@ -1,6 +1,7 @@
 class InputParametersController < ApplicationController
   #Ruby logic should always be here - none in the view page
   def parameter_input
+    #/test_page (page)
     #p params[:name]
     @param_value = params[:name].upcase
     if @name[0]=='A'
@@ -10,17 +11,19 @@ class InputParametersController < ApplicationController
   end
 
   def query_name
+    #/say_name (page)
     #Make a counter
     @counter = params[:count].to_i
     render 'name_of_query.html.erb'
   end
 
   def guess_number
+    #Page needs to be made, yet
     @guess = params[:guess].to_i
     if @guess != 0 and @guess != "0" 
       if @guess.to_i>50
         @message = "You guessed too high."
-      elseif @guess.to_i<50
+      elsif @guess.to_i<50
         @message = "You guessed too low."
       else
         @message = "You got it!"
@@ -34,31 +37,47 @@ class InputParametersController < ApplicationController
     render 'url_seg_param_view.html.erb'
   end
 
+  def guessing_game_form
+    #/guess_the_number (page)
+    #This online form prompts a user to guess a number
+    render 'guessing_game_form.html.erb'
+  end
+  
   def guessing_game
+    #/submit_guess and /guessing_game (pages)
+    #this method takes in a number from a form and 
+    #returns a response (/submit_guess page), based on whether 
+    #the number matches a randomly generated number
     @message = "You guessed too " 
-    @guess = params[:guess].to_i
+    #@guess is assigned the number entered in the input box
+    @guess = params[:guessed_number].to_i
+    @num = rand(0...100)+1
     if @guess != 0 and @guess != "0" 
-      if @guess < 1
-        @message = "Really?"
-      elsif @guess.to_i>50
-        @message = "You guessed too high."
-      elseif @guess.to_i<50
-        @message = "You guessed too low."
-      else
+      if @guess.to_i > @num
+        @message += "high."
+      elsif @guess.to_i < @num
+        @message += "low."
+      elsif @guess == @num
         @message = "You got it!"
       end
+    else
+      @message = "Type a number, not a word, or 0.."
     end  
-    render 'guess_a_num.html.erb'
+    render 'guessing_game_submit_page.html.erb'
   end
 
   def form_params
-    #my first and last names become the keys to the param from this
+    #/form (page)
+    #my first and last names become the keys 
+    #to the params from this form, which are
+    #passed to the submit-button method
     #@form_1 = params[:first_name]
     #@form_2 = params[:last_name]
     render 'form_params.html.erb'
   end
 
   def submit_button
+    #/submit_form (page)
     #Use the form input to render it on the submit page
     @form_1 = params[:first_name]
     @form_2 = params[:last_name]
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #/test_page is the web page; input_parameters refers to input_parameters_controller.rb;
+  #parameter_input refers to the method in input_parameters_controller.rb, which renders /test_page
   get '/test_page'=>'input_parameters#parameter_input'
   get '/say_name'=>'input_parameters#query_name'
-  get '/guess_number'=>'input_parameters#guessing_game'
+  #the number guessing game form page
+  get '/guess_the_number'=> 'input_parameters#guessing_game_form'
+  #the number guessing submittal page
+  get '/submit_guess'=>'input_parameters#guessing_game'
+  #the results page that shows after submittal (redundant?)
+  post'/guessing_game'=>'input_parameters#guessing_game'
+
+  #form prototype page - takes you to the form_params method in the input_parameters_controller.rb file, which renders the /form page (form_params.html.erb)
   get '/form'=> 'input_parameters#form_params'
+
   #Colon syntax is ONLY allowed in the routes file
+  
   get '/url_segment_parameter/:this_is_a_variable'=>'input_parameters#url_segment_parameter_method'
-  post'/submit_form'=>'input_parameters#submit_button'
+  post'/submit_form'=>'input_parameters#submit_button'  
 end
